ab471447d4de2e6d265f06376de482b88db3613f
snelusha <hello@snelusha.dev>
2025-10-25 21:59:04 +0530

    feat: add homebrew formula

---
+++ Formula/noto.rb
@@ -0,0 +1,18 @@
+class Noto < Formula
+  desc "Generate clean commit messages in a snap! ✨"
+  homepage "https://noto.snelusha.dev"
+  url "https://registry.npmjs.org/@snelusha/noto/-/noto-1.2.9.tgz"
+  sha256 "d4b9ccc7f84efdf6e21d037b847d242777ad967b44235914d16b8ec1a7f218ac"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/noto", "--version"
+  end
+end
